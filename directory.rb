144fda1989e72764d9a45aed51d9ce7981aad111
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-07 19:00:14 +0000

    Refractored

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 9
 # first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "Berta"
@@ -17,7 +18,5 @@ puts "Makis"
 puts "Nabin"
 puts "Peter"
 # finally, we print the total
-print "Overall, we have "
-# it's important that print() doesn't add new line characters
-print 9 
-puts " great students"
+puts "Overall, we have #{student_count} great studetns"
+
