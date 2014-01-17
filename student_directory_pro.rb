4246dc57bccf4be5b9dd0bdac8de8c01e976f6ff
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-17 15:27:26 +0000

    added a save students method to menu

--- student_directory_pro.rb
+++ student_directory_pro.rb
@@ -29,7 +29,7 @@ def input_students
 	while !name.empty? do
 		# add the student hash to the array
 		@students << {:name => name, :cohort => :january}
-		puts "Now we have #{students.length} students"
+		puts "Now we have #{@students.length} students"
 		# get another name from the user
 		name = gets.chomp
 	end
@@ -41,6 +41,7 @@ end
 def print_menu
 		puts "1. Input the students"
 		puts "2. Show the students"
+    puts "3. Save the students"
 		puts "9. Exit"
 	end
 
@@ -62,6 +63,8 @@ def interactive_menu
 			@students = input_students
 		when "2"
 			show_students
+    when "3"
+      save_students
 		when "9"
 			exit # thi will casue the program to terminate
 		else 
@@ -69,6 +72,18 @@ def interactive_menu
 			"I don't know what you meant, try again"
 		end
 	end
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 
